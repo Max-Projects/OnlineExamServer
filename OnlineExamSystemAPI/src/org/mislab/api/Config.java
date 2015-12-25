7a8da5727ccc62862d5f18f157a4690e8168aa9c
Malthael <ys111110111@gmail.com>
2015-12-25 23:06:38 +0800

    change host IP to Amazon EC2

--- OnlineExamSystemAPI/src/org/mislab/api/Config.java
+++ OnlineExamSystemAPI/src/org/mislab/api/Config.java
@@ -1,6 +1,6 @@
 package org.mislab.api;
 
 public interface Config {
-    public static final String HOST = "http://localhost:8000";
+    public static final String HOST = "http://52.192.15.18:2301";
     public static final int PORT = 3001;
 }
