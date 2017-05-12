57b8b38546c944068cf0b31dfa6d2b9362d19707
max <i.max.yuan@gmail.com>
2017-05-12 16:36:25 +0800

    Update to the latest

--- OnlineExamSystemAPI/src/org/mislab/api/Config.java
+++ OnlineExamSystemAPI/src/org/mislab/api/Config.java
@@ -1,6 +1,12 @@
 package org.mislab.api;
 
 public interface Config {
-    public static final String HOST = "http://192.168.0.10:8000";
-    public static final int PORT = 3001;
+//    public static final String HOST = "http://192.168.0.10:8000";
+    public static String ServerURL = "http://172.16.96.169:8000";
+    public static String LocalIP = "http://192.168.0.1";
+    public static int PORT = 3001;
+    public static String LocalURL = LocalIP + ":" + PORT;
+    
+    public static String snapshotScale = "100"; // in percentage to the original
+    public static String snapshotFreq = "1000"; // in milliseconds
 }
